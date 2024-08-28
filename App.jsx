0fd6ff204b129d325e1b2841ee390c8e9d46d2cd
OmPatel <116576083+OmPatel123456@users.noreply.github.com>
2024-08-28 18:15:08 -0400

    Create App.jsx
    Signed-off-by: OmPatel <116576083+OmPatel123456@users.noreply.github.com>

---
+++ App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
+  const [allClicks, setAll] = useState([])
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
+    </div>
+  )
+}
+
+export default App
+
